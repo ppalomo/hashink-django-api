4a52089f1821a941ec579e88efc0714e947f2da6
ppalomo <ppalomov@gmail.com>
2021-05-03 16:38:50 +0200

    Adding DEBUG variable to settings

--- hashink_api/settings.py
+++ hashink_api/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '$rbpul+jz(ux=4!2jo94pkly1tx8@o&1al3au^1-94l12&)n2o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', True)
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
